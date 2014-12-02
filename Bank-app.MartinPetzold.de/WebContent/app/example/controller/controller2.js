7731ade93068b4d0a62ec29c9c8ba5b80dcfd9aa
Aismael <Aismaelinc@web.de>
2014-12-02 17:25:16 +0100

    own json file

--- Bank-app.MartinPetzold.de/WebContent/app/example/controller/controller2.js
+++ Bank-app.MartinPetzold.de/WebContent/app/example/controller/controller2.js
@@ -6,7 +6,7 @@ var phonecatApp = angular.module('phonecatApp2', []);
 phonecatApp.controller('PhoneListCtrl2', ['$scope', '$http',
                                           
   function ($scope, $http) {
-    $http.get('https://raw.githubusercontent.com/angular/angular-phonecat/24a13ca0a0513d6b3c0d8d770cef0cb094d4aab9/app/phones/phones.json').success(function(data) {
+    $http.get('https://raw.githubusercontent.com/Aismael/AngularjsBank/master/Bank-app.MartinPetzold.de/WebContent/app/example/data/phones.json').success(function(data) {
       $scope.phones = data;
       $scope.status=true;
     }).error(function(data, status, headers, config) {
