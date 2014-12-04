232486b656ea45861a72c9fd5bc19fff3ae9e8de
Martin.Petzold <Aismaelinc@web.de>
2014-12-04 16:29:00 +0100

    first projekt step get and put for banks

---
+++ test/WebContent/app/example/controller/bankController.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/* Controllers */
+var Bankapp = angular.module('bankapp', []);
+
+Bankapp
+		.controller(
+				'BankListCtrl',
+				[
+						'$scope',
+						'$http',
+
+						function($scope, $http) {
+							$scope.loadData= function() {
+							$http
+									.get(
+											'http://localhost:8080/bank-swt-neu-vor-14/rest/banktut')
+									.success(function(data) {
+										$scope.banks = data;
+										$scope.status = true;
+									}).error(
+											function(data, status, headers,
+													config) {
+												$scope.status = false;
+											});
+							};
+							$scope.loadData();
+							$scope.orderProp = 'name';
+							
+							$scope.add = function() {
+								$http({
+									withCredentials : false,
+									method : 'post',
+									url : 'http://localhost:8080/bank-swt-neu-vor-14/rest/banktut',
+									data : {
+										name : $scope.name,
+										sortCode : $scope.blz
+									}
+								}).success(function() {
+									$scope.loadData();
+								})
+								
+							};
+							$scope.refresh = function() {$scope.loadData();};
+						}
+
+				]);
